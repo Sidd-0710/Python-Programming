a634894a8d7f0adfbacad0e64289fd80b8cdbef5
Siddheshwar Chirkute <siddheshwarchirkute2003@gmail.com>
2026-09-12 16:47:48 +0530

    Learning Varaiables in Python

--- FirstCode.py
+++ FirstCode.py
@@ -1,1 +1,3 @@
-print("Hello, World!")
+print("i like python")
+print("its really fun to learn")
+print("i am enjoying learning python")
---
+++ var.py
@@ -0,0 +1,27 @@
+# # Variable = a container for a value. The value can be of any type, including another variable. Variables are created when you assign a value to them.
+
+# first_name = "Sidd"  # String variable
+# last_name = "Chirkute"
+# full_name = first_name + " " + last_name
+# print("Hello "+ full_name)
+# print (type(first_name))
+
+
+# age = 22
+# age += 1
+# print(age)
+# print(type(age))
+# print("your age is :"+str(age))
+
+
+
+# height = 250.5
+# print(height)
+# print(type(height))
+# print("Your Height is :"+str(height)+"cm")
+
+
+# human = True
+# print(human)
+# print(type(human))
+# print("Are you a human: "+str(human))
